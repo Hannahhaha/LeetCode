daf612d91251f08a63174e5e6c90df8ccd1b9deb
Hannahhaha <1428824570@qq.com>
2020-07-21 13:50:07 +0800

    Update

--- 7.20/DelRep2.cpp
+++ 7.20/DelRep2.cpp
@@ -12,7 +12,8 @@ public:
 
         if(n<=2)
          return n;
-        
+        //慢指针从第2位开始，快指针从第三位开始，如果快指针不等于慢指针的前一位元素（保证中间空一个元素，可以保证元素最多出现两次）
+        //那么那么慢指针的下一位就要赋上快指针的值，此时将快指针向下一位移动，重复上面的逻辑，直到快指针到达数组的最后 
         for(int fast=2;fast<n;fast++)
         {
             if(nums[fast]!=nums[slow-1])
@@ -20,7 +21,7 @@ public:
                 nums[++slow]=nums[fast];
             }
         }
-        return slow+1;
+        return slow+1;//慢指针长度+1，即处理后数组的长度
     }
 
 };
