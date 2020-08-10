26b25a4cf3d98f1351c6cfb678eec3200f320e52
Hannahhaha <1428824570@qq.com>
2020-08-10 19:12:51 +0800

    Chat

---
+++ MultiThreadSC/Source/MultiThreadSC/MultiThreadSC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiThreadSC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiThreadSC, "MultiThreadSC" );
